c15fe2c9741a795e766b609d8fb56b2f89163481
GitHub-MXB <43258968+GitHub-MXB@users.noreply.github.com>
2019-05-09 21:38:12 +0800

    2019-5-9

--- js/index.js
+++ js/index.js
@@ -16,11 +16,50 @@ ready(function () {
     console.timeEnd("time");
     console.log(b);
 });
+var dataArr = [];
+
+function setData(cls, key, value) {
+    var let1 = data[cls];
+    switch (typeof let1[key]) {
+        case "object":
+            let1[key].value = value;
+            break;
+        case "boolean":
+            if (value) {
+                let1[key] = key;
+            } else {
+                let1[key] = "";
+            }
+            break;
+        default:
+            let1[key] = value;
+            break;
+    }
+    dataArr.push(cls, key);
+}
+var arr = ["link", "href"];
+setData("link", "href", "百度");
+
+function addr(arr) {
+    return data2[arr[0]][arr[1]];
+}
+console.time("time");
+var a = addr(arr);
+console.timeEnd("time");
+console.log(a, typeof true);
 
 function getNode(dom) {
     var let1 = dom.childNodes[2].nodeValue;
     return let1;
 }
+
+function set(fn1) {
+    fn1();
+    fn2(); //处理给data2
+}
+set(function (self) { //用户的操作写这里
+    self.href = "a"; //setData
+});
 
 function getAttr(dom) {
     var let1 = dom.attributes;
