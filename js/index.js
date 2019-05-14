e24bd12192a3f78e0f54f17cacb0fa70562610bc
GitHub-MXB <43258968+GitHub-MXB@users.noreply.github.com>
2019-05-14 21:57:38 +0800

    2019-5-14 end

--- js/index.js
+++ js/index.js
@@ -2,44 +2,16 @@ ready(function () {
     Ajax("GET", "ajax.txt", "", function (e) {
         console.log(e);
     });
-    // var typ = document.createAttribute("isMould");
-    // typ.nodeValue = "";
-    // attr.setNamedItem(typ);
-    console.time("a");
-    dom1 = dom(document.body, "link")[0];
-    dom2 = dom1.cloneNode(true);
-    attr = dom2.attributes;
-    attr.href.nodeValue = data_dom.link.href;
-    console.timeEnd("a");
-    console.log(dom1, dom2);
-
-    arr = node_array(dom1);
-    console.log(arr, dom(document.body, "link")[0]);
+    console.log(data, data_dom, dom_data);
+    console.time("time");
     React();
-});
-var dataArr = [];
-
-function getNode(dom) {
-    var let1 = dom.childNodes[2].nodeValue;
-    return let1;
-}
-
-data.link.setData(function () { //用户的操作写这里
-    this.map.value = 50;
-    this.map.fn = function () {
-        return this.map.value + 50;
-    };
-});
-console.log(data, data_dom);
-
-
-function getAttr(dom) {
-    var let1 = dom.attributes;
-    var let2 = {};
-    forEach(let1, function (i, node) {
-        let2[node.nodeName] = node.nodeValue; //将dom的值转化为data
-        //到时只需一次就可以获取数据
+    setData(function () { //用户的操作写这里
+        this.link.setData(function () {
+            this.map.value = 50;
+            this.map.fn = function () {
+                return this.map.value + 50;
+            };
+        });
     });
-    return let2;
-}
-console.timeEnd("time");
+    console.timeEnd("time");
+});
--- js/js.js
+++ js/js.js
@@ -7,7 +7,6 @@ function ready(fn) {
     } else {
         document.attachEvent('onreadystatechange', function () {
             if (document.readyState == 'complete') {
-                // complete  interactive
                 document.detachEvent('onreadystatechange', arguments.callee);
                 fn(); //函数执行
             }
@@ -59,7 +58,152 @@ function forEach(obj, fn, index) {
         }
     }
 }
+//用时，即赋值时就完成处理，赋值一次只处理一个，不要取的时候处理
+Object.prototype.setData1 = function (value, addr, _addr) {
+    _addr = this;
+    forEach(value, function (value, key) {
+        if (String(key).charAt(0) == "_") {
+            return;
+        }
+        if (value === null) {
+            addr[key] = "";
+            return;
+        }
+        switch (typeof value) {
+            case "object":
+                if (value instanceof Array) {
+                    addr[key] = value;
+                    break;
+                }
+                addr[key] = {};
+                this.setData1(value, addr[key]);
+                break;
+            case "boolean":
+                addr[key] = value ? key : "";
+                break;
+            case "function":
+                addr[key] = value.call(_addr);
+                break;
+            default:
+                addr[key] = value;
+                break;
+        }
+    });
+};
+var domArr = [];
+Object.prototype.setData = function (fn) {
+    fn.call(this);
+    this.setData1(this, this._addr[0]);
+    domArr.push(this);
+};
 
-// function dom(str) {
-//     return document.getElementById(str);
-// }
+function setData(fn, data_dom, dom_data, dom, attr, text, str) {
+    fn.call(data);
+    forEach(domArr, function (value, key) { //这里更新dom，即domArr
+        data_dom = value._addr[0];
+        dom_data = value._addr[1];
+        dom = window.dom(document.body, value._addr[2])[0];
+        forEach(dom_data, function (value, key) {
+            attr = dom.attributes;
+            text = dom.childNodes;
+            forEach(value.attr, function (value, key) {
+                str = getData(value, data_dom);
+                if (attr[key].nodeValue != str) {
+                    attr[key].nodeValue = str;
+                }
+            });
+            forEach(value.text, function (value, key) {
+                str = getData(value, data_dom);
+                if (text[key].nodeValue != str) {
+                    text[key].nodeValue = str;
+                }
+            });
+            str = value['class'];
+            if (str) {
+                str = getData(str, data_dom);
+                if (dom.className != str) {
+                    dom.className = str;
+                }
+            }
+        });
+    });
+    domArr = [];
+}
+data = {};
+data_dom = {};
+
+function node_array(dom, arr) {
+    function allNode(node, arr) {
+        if (node.attributes.mould) {
+            return;
+        }
+        forEach(node.children, function (value, key) {
+            arr.push(value);
+            allNode(value, arr);
+        });
+    }
+    if (!dom)
+        return;
+    arr = [dom];
+    allNode(dom, arr);
+    return arr;
+}
+
+function Vue(obj) {
+    data[obj.el] = obj.data;
+    data_dom[obj.el] = {};
+    dom_data[obj.el] = dom_data[obj.el] || {};
+    data[obj.el]._addr = [data_dom[obj.el], dom_data[obj.el], obj.el];
+    setData(function () {});
+}
+dom_data = {};
+
+function React(arr, This, This2, str) {
+    forEach(dom_data, function (value, key) {
+        This = value;
+        arr = node_array(dom(document.body, key)[0]);
+        forEach(arr, function (value, key) {
+            This[key] = {};
+            This2 = This[key];
+            This2.attr = {};
+            forEach(value.attributes, function (value, key) {
+                // value.nodeValue;
+                //遍历完，转移class和mould
+                str = value.nodeValue + ""; //ie迷之错误,||出错
+                if (str.indexOf("{") >= 0) {
+                    This2.attr[value.nodeName] = str;
+                }
+            });
+            This2.text = {};
+            forEach(value.childNodes, function (value, key) {
+                if (value.nodeType == 3) {
+                    str = value.nodeValue + "";
+                    if (str.indexOf("{") >= 0) {
+                        This2.text[key] = str;
+                    }
+                }
+            });
+            str = This2.attr['class'];
+            if (str) {
+                This2['class'] = str;
+                delete This2.attr['class'];
+            }
+            str = This2.attr.mould;
+            if (str) {
+                This2.mould = str;
+                delete This2.attr.mould;
+            }
+        });
+    });
+}
+
+function getData(obj, c) { //先用这个，以后换成fn2
+    return obj.replace(/{([^}]+)}/g, function (a, b) { //   
+        if (b && c) {
+            forEach(b.split("."), function (value, key) {
+                c = c[value] || "";
+            });
+        }
+        return c;
+    });
+}
